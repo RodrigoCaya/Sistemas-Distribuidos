8703f19ffba52ca8c7a683999b274e1999322453
root <root@dist13.inf.santiago.usm.cl>
2020-10-13 20:28:21 -0300

    adding helloworld again

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/RodrigoCaya/Sistemas-Distribuidos
+
+go 1.15
+
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	golang.org/x/net v0.0.0-20201010224723-4f7140c49acb
+	golang.org/x/sys v0.0.0-20201013132646-2da7054afaeb // indirect
+	google.golang.org/genproto v0.0.0-20201013134114-7f9ee70cb474 // indirect
+	google.golang.org/grpc v1.33.0
+	google.golang.org/protobuf v1.25.0
+)
--- server.go
+++ server.go
@@ -11,9 +11,9 @@ func main(){
 	if err != nil {
 		log.Fatalf("Failed to listen on port 9000: %v", err)
 	}
-	s := chat.Server{}
+	s := helloworld.Server{}
 	grpcServer := grpc.NewServer()
-	objeto.RegisterChatServiceServer(grpcServer, &s)
+	helloworld.RegisterChatServiceServer(grpcServer, &s)
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
 	}
