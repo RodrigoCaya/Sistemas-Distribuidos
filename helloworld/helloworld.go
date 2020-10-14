b1c5d9a46795be4c74f0f9b312d27f5aae2d9c84
Rodrigo Cayazaya <rodrigo.cayazaya@sansano.usm.cl>
2020-10-14 18:37:30 -0300

    ahora si ctm

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -10,6 +10,6 @@ type Server struct{
 }
 
 func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, error){
-	log.Printf("Received message body from client: %s", message.Id)
+	log.Printf("Received message body from client: %s %s %s %s %s %s", message.Id, message.Producto, message.Valor, message.Tienda, message.Destino, message.Propietario)
 	return &Message{Id: "Hello From the Server!"}, nil
 }
