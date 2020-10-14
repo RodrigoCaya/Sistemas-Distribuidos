b6f77657088c55f0e66b57c7856662b08f6eedf6
Rodrigo Cayazaya <rodrigo.cayazaya@sansano.usm.cl>
2020-10-14 20:10:07 -0300

    agregado retail

--- client.go
+++ client.go
@@ -22,9 +22,7 @@ type pymes struct {
 }
 
 
-func pym(conn *grpc.ClientConn){
-	//leer pymes
-	
+func pym(conn *grpc.ClientConn){	
 	f, err := os.Open("pymes/pymes.csv")
 	if err != nil{
 		log.Printf("error abriendo el archivo: %v", err)
@@ -49,14 +47,51 @@ func pym(conn *grpc.ClientConn){
 			log.Printf("error leyendo la linea: %v", err)
 		}
 
-		// msj := pymes{
-		// 	id: record[0],
-		// 	producto: record[1],
-		// 	valor: record[2],
-		// 	tienda: record[3],
-		// 	destino: record[4],
-		// 	propietario: record[5],
-		// }
+		c := helloworld.NewHelloworldServiceClient(conn)
+		
+		message := helloworld.Message{
+			Id: record[0],
+			Producto: record[1],
+			Valor: record[2],
+			Tienda: record[3],
+			Destino: record[4],
+			Propietario: record[5],
+		}
+
+		response, err := c.SayHello(context.Background(), &message)
+		if err != nil {
+			log.Fatalf("Error when calling SayHello: %s", err)
+		}
+
+		log.Printf("Response from Server: %s", response.Id)
+		time.Sleep(2 * time.Second)
+	}
+}
+
+func ret(conn *grpc.ClientConn){	
+	f, err := os.Open("retail/retail.csv")
+	if err != nil{
+		log.Printf("error abriendo el archivo: %v", err)
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+	r.Comma = ','
+	r.FieldsPerRecord = 6
+
+	if _, err := r.Read(); err != nil{
+		panic(err)
+	}
+
+	for{
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Printf("error leyendo la linea: %v", err)
+		}
 
 		c := helloworld.NewHelloworldServiceClient(conn)
 		
@@ -88,5 +123,6 @@ func main() {
 	}
 	defer conn.Close()
 	pym(conn)
+	ret(conn)
 	
 }
