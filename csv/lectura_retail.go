9599a29a25fad7e19cc8137404c2395a0a0fdeb3
Jean-Franco <55410224+Jean-Franco@users.noreply.github.com>
2020-10-14 16:07:23 -0300

    lecturas

---
+++ csv/lectura_retail.go
@@ -0,0 +1,66 @@
+package main
+
+import(
+	"os"
+	"log"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"strconv"
+)
+
+type retail struct {
+	id string
+	producto string
+	valor int
+	tienda string
+	destino string
+}
+
+func hola(){
+
+	f, err := os.Open("retail.csv")
+	if err != nil{
+		log.Printf("error abriendo el archivo: %v", err)
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+	r.Comma = ','
+	r.FieldsPerRecord = 5
+
+	if _, err := r.Read(); err != nil{
+		panic(err)
+	}
+
+
+	var retails []retail
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Printf("error leyendo la linea: %v", err)
+		}
+
+		p := retail{
+			id: record[0],
+			producto: record[1],
+			tienda: record[3],
+			destino: record[4],
+		}
+
+		if record[2] != ""{
+			i, err := strconv.Atoi(record[2])
+			if err != nil{
+				log.Printf("error al procesar el valor: %v", err)
+				continue
+			}
+			p.valor = i
+		}
+
+		retails = append(retails, p)
+	}
+	fmt.Println(retails)
+}
