9730c0059cb1c0e9734ab7838aac2639d247fc9a
root <root@dist13.inf.santiago.usm.cl>
2020-10-13 21:35:49 -0300

    adding client

---
+++ client.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	"github.com/RodrigoCaya/Sistemas-Distribuidos/helloworld"
+)
+
+func main() {
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %s", err)
+	}
+	defer conn.Close()
+
+	c := helloworld.NewHelloworldServiceClient(conn)
+	
+	message := helloworld.Message{
+		Body: "hola jean",
+	}
+
+	response, err := c.SayHello(context.Background(), &message)
+	if err != nil {
+		log.Fatalf("Error when calling SayHello: %s", err)
+	}
+
+	log.Printf("Response from Server: %s", response.Body)
+	
+}
