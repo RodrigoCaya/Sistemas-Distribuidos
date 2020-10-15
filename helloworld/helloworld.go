b14988574f1e166cefdc1186893ac408730f9f8f
Rodrigo Cayazaya <rodrigo.cayazaya@sansano.usm.cl>
2020-10-15 12:21:56 -0300

    fixing registro.csv

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -13,18 +13,18 @@ type Server struct{
 var cont int = 0
 
 func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, error){
+	log.Printf("%s %s %s %s %s %s %s", message.Id, message.Producto, message.Valor, message.Tienda, message.Destino, message.Propietario, message.Estado)
+	cont = cont + 1
+	result := message.Producto + " es: " + strconv.Itoa(cont)
+	
 	f, err := os.OpenFile("csv/registro.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
     if err != nil {
         log.Fatal(err)
 	}
-	_, err = f.Write([]byte(message.Id+","+message.Producto+","+message.Valor+","+message.Tienda+","+message.Destino+","+message.Propietario+","+message.Estado))
+	_, err = f.Write([]byte(strconv.Itoa(cont)+","+message.Id+","+message.Producto+","+message.Valor+","+message.Tienda+","+message.Destino+","+message.Propietario+","+message.Estado+"\n"))
     if err != nil {
         log.Fatal(err)
 	}
 	f.Close()
-
-	log.Printf("%s %s %s %s %s %s %s", message.Id, message.Producto, message.Valor, message.Tienda, message.Destino, message.Propietario, message.Estado)
-	cont = cont + 1
-	result := message.Producto + " es: " + strconv.Itoa(cont)
 	return &Message{Id: result}, nil
 }
