ee1d40ed77b5b68780f1d7c620cfa026a9155d3d
Rodrigo Cayazaya <rodrigo.cayazaya@sansano.usm.cl>
2020-10-14 23:02:06 -0300

    fixing

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -14,6 +14,6 @@ var cont int = 0
 func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, error){
 	log.Printf("%s %s %s %s %s %s", message.Id, message.Producto, message.Valor, message.Tienda, message.Destino, message.Propietario)
 	cont = cont + 1
-	result := message.Producto + "es" + strconv.Itoa(cont)
+	result := message.Producto + " es " + strconv.Itoa(cont)
 	return &Message{Id: result}, nil
 }
