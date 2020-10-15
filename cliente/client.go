3c6293d32616d80e5e687b3256f6b881a2ec80ea
Jean-Franco <55410224+Jean-Franco@users.noreply.github.com>
2020-10-15 19:14:15 -0300

    en vola explota

--- cliente/client.go
+++ cliente/client.go
@@ -16,7 +16,7 @@ import (
 
 
 func pym(conn *grpc.ClientConn, tiempo int){	
-	f, err := os.Open("csv/pymes.csv")
+	f, err := os.Open("../csv/pymes.csv")
 	if err != nil{
 		log.Printf("error abriendo el archivo: %v", err)
 	}
@@ -80,7 +80,7 @@ func pym(conn *grpc.ClientConn, tiempo int){
 }
 
 func ret(conn *grpc.ClientConn, tiempo int){	
-	f, err := os.Open("csv/retail.csv")
+	f, err := os.Open("../csv/retail.csv")
 	if err != nil{
 		log.Printf("error abriendo el archivo: %v", err)
 	}
