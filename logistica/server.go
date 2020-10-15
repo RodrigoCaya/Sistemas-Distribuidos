aa66bd9c1266b8e73ceee681268fe0e406958e32
Jean-Franco <55410224+Jean-Franco@users.noreply.github.com>
2020-10-15 20:24:42 -0300

    mmm

--- logistica/server.go
+++ logistica/server.go
@@ -6,22 +6,37 @@ import(
 	"github.com/RodrigoCaya/Sistemas-Distribuidos/helloworld"
 )
 
-func main(){
+func conexioncl(){
 	liscliente, err := net.Listen("tcp", ":9000")
-	liscamion, err2 := net.Listen("tcp", ":9001")
+	
 	if err != nil {
 		log.Fatalf("Failed to listen on port 9000: %v", err)
 	}
-	if err2 != nil {
-		log.Fatalf("Failed to listen on port 9001: %v", err2)
-	}
+	
 	s := helloworld.Server{}
 	grpcServer := grpc.NewServer()
 	helloworld.RegisterHelloworldServiceServer(grpcServer, &s)
 	if err := grpcServer.Serve(liscliente); err != nil {
 		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
 	}
+}
+
+func conexionca(){
+	liscamion, err2 := net.Listen("tcp", ":9001")
+	if err2 != nil {
+		log.Fatalf("Failed to listen on port 9001: %v", err2)
+	}
+	s := helloworld.Server{}
+	grpcServer := grpc.NewServer()
+	helloworld.RegisterHelloworldServiceServer(grpcServer, &s)
+
 	if err2 := grpcServer.Serve(liscamion); err2 != nil {
 		log.Fatalf("Failed to serve gRPC server over port 9001: %v", err2)
 	}
+}
+
+func main(){
+	go conexioncl()
+	go conexionca()
+	
 }
