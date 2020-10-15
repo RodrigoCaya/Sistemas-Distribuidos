c7cc2112ffe4ae04bd354f05f21d324a38d00ea9
Rodrigo Cayazaya <rodrigo.cayazaya@sansano.usm.cl>
2020-10-14 22:54:34 -0300

    adding cont

--- helloworld/helloworld.go
+++ helloworld/helloworld.go
@@ -2,14 +2,17 @@ package helloworld
 
 import (
 	"log"
-
+	"strconv"
 	"golang.org/x/net/context"
 )
 
 type Server struct{
 }
 
+var cont int = 0
+
 func (s *Server) SayHello(ctx context.Context, message *Message) (*Message, error){
 	log.Printf("Received message body from client: %s %s %s %s %s %s", message.Id, message.Producto, message.Valor, message.Tienda, message.Destino, message.Propietario)
-	return &Message{Id: "Hello From the Server!"}, nil
+	cont = cont + 1
+	return &Message{Id: strconv.Itoa(cont)}, nil
 }
