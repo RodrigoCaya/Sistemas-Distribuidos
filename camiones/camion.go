7c5e91a57b22ce85c95305cef8eb46f4cd70f816
root <root@dist13.inf.santiago.usm.cl>
2020-10-16 02:09:21 -0300

    adding cases

--- camiones/camion.go
+++ camiones/camion.go
@@ -17,17 +17,42 @@ type Camion struct{
 
 var camiones []Camion
 
-func conectar(i int, c helloworld.HelloworldServiceClient){
-	message := helloworld.PaqueteRequest{
-		Idcamion: camiones[i].id,
-		Tipo: camiones[i].tipo,
-	}
-	log.Printf("Camión "+camiones[i].id+" se está preparando")
+// func delivery(){
+	//todo
+	// cambiar disponibilidad a 1 al volver
+// }
+
+func conectar(i int, c helloworld.HelloworldServiceClient, tiempo int){
+	for{
+		message := helloworld.PaqueteRequest{
+			Idcamion: camiones[i].id,
+			Tipo: camiones[i].tipo,
+		}
+		log.Printf("Camión "+camiones[i].id+" se está preparando")
 		response, err := c.EnviarPaquete(context.Background(), &message)
 		if err != nil {
 			log.Fatalf("Error when calling EnviarPaquete: %s", err)
 		}
-		log.Printf("El camión %s tomó el paquete %s",camiones[i].id ,response.Idpaquete)
+		if response.Idpaquete != "No hay más paquetes" {
+			log.Printf("El camión %s tomó el paquete %s con código %s",camiones[i].id ,response.Idpaquete, response.Seguimiento)
+			camiones[i].espacio = camiones[i].espacio + 1
+		}else{
+			log.Printf("No hay más paquetes para el camión %s",camiones[i].id)
+			if camiones[i].espacio == 1{
+				log.Printf("El camión %s fue a hacer su entrega",camiones[i].id)
+				camiones[i].disponibilidad = 0
+				//delivery()
+			}else if camiones[i].espacio == 0 {
+				log.Printf("El camión %s se ha quedado vacío",camiones[i].id)
+			}
+		}
+		if camiones[i].espacio == 2 {
+			log.Printf("El camión %s fue a hacer su entrega",camiones[i].id)
+			camiones[i].disponibilidad = 0
+			//delivery()
+		}
+		time.Sleep(time.Duration(tiempo) * time.Second)
+	}
 }
 
 func main(){
@@ -68,14 +93,18 @@ func main(){
 	fmt.Println("Indique el intervalo de tiempo de espera de los camiones: ") 
 	var first string 	  
 	fmt.Scanln(&first)
-
+	i, err := strconv.Atoi(first)
+	if err != nil{
+		log.Printf("error al ingresar el valor: %v", err)
+		continue
+	}
 	log.Printf("Los camiones estan listos para partir")
 
-	go conectar(1,c)
+	go conectar(1,c,i)
 
-	go conectar(2,c)
+	go conectar(2,c,i)
 
-	conectar(0,c)
+	conectar(0,c,i)
 
 	
 
