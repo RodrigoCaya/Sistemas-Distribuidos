1f603c753c9b11f5b63d983cf1e766841c8571b2
Rodrigo Cayazaya <rodrigo.cayazaya@sansano.usm.cl>
2020-10-15 22:35:48 -0300

    deleting go in server.go

--- cliente/client.go
+++ cliente/client.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"encoding/csv"
 	"io"
+	"context"
 		
 	"google.golang.org/grpc"
 
--- logistica/server.go
+++ logistica/server.go
@@ -36,7 +36,7 @@ func conexionca(){
 }
 
 func main(){
-	go conexioncl()
-	go conexionca()
+	conexioncl()
+	// conexionca()
 	
 }
