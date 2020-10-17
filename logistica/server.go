c8dc144fe57e402237373fe1302621239fd44a00
root <root@dist14.inf.santiago.usm.cl>
2020-10-17 19:33:41 -0300

    cambiando server rabbit

--- logistica/server.go
+++ logistica/server.go
@@ -6,6 +6,7 @@ import(
 	"github.com/RodrigoCaya/Sistemas-Distribuidos/helloworld"
 	"github.com/streadway/amqp"
 	"fmt"
+	"time"
 )
 
 func conexioncl(){
@@ -37,6 +38,8 @@ func conexionca(){
 	}
 }
 
+
+
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Fatalf("%s: %s", msg, err)
@@ -44,38 +47,51 @@ func failOnError(err error, msg string) {
 	}
 }
 
-
 func main(){
 	go conexioncl()
-	conexionca()
+	go conexionca()
+
+	//conexion
 	conn, err := amqp.Dial("amqp://test:test@10.6.40.154:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
+	//canal
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
+	//cola
 	q, err := ch.QueueDeclare(
 		"hello-queue", // name
-		false,         // durable
-		false,         // delete when unused
-		false,         // exclusive
-		false,         // no-wait
-		nil,           // arguments
+		false,   // durable
+		false,   // delete when usused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
-	body := "{name:arvind, message:hello}"
-	err = ch.Publish(
-		"",     // exchange
-		q.Name, // routing key
-		false,  // mandatory
-		false,  // immediate
-		amqp.Publishing{
-			ContentType: "application/json",
-			Body:        []byte(body),
-		})
-	log.Printf(" [x] Sent %s", body)
-	failOnError(err, "Failed to publish a message")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
 }
 
